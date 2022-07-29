afcc0899e05ff7d0dccaee1a71b638638ea81654
persianpros <persianpros@yahoo.com>
2022-07-29 14:32:43 +0430

    six cleanup

--- plugin/compat.py
+++ plugin/compat.py
@@ -3,40 +3,28 @@
 #Codes for Taapat (Youtube plugin)
 
 from re import compile
-from sys import version_info
-
-PY3 = version_info[0] == 3
+from six.moves.urllib.parse import urlencode as compat_urlencode
+from six.moves.urllib.parse import quote as compat_quote
+from six.moves.urllib.parse import unquote_to_bytes as compat_unquote_to_bytes
+from six.moves.urllib.request import urlopen as compat_urlopen
+from six.moves.urllib.request import Request as compat_Request
+from six.moves.urllib.error import URLError as compat_URLError
+from six.moves.urllib.parse import urljoin as compat_urljoin
+from six.moves.urllib.parse import urlparse as compat_urlparse
+from six import PY3
 
 if PY3:
 	# Python 3
 	compat_str = str
-	from urllib.parse import urlencode as compat_urlencode
-	from urllib.parse import quote as compat_quote
-	from urllib.parse import unquote_to_bytes as compat_unquote_to_bytes
-	from urllib.request import urlopen as compat_urlopen
-	from urllib.request import Request as compat_Request
-	from urllib.error import URLError as compat_URLError
-	from urllib.parse import urljoin as compat_urljoin
-	from urllib.parse import urlparse as compat_urlparse
 else:
 	# Python 2
 	compat_str = unicode
-	from urllib import urlencode as compat_urlencode
-	from urllib import quote as compat_quote
-	from urllib import unquote as compat_unquote_to_bytes
-	from urllib2 import urlopen as compat_urlopen
-	from urllib2 import Request as compat_Request
-	from urllib2 import URLError as compat_URLError
-	from urlparse import urljoin as compat_urljoin
-	from urlparse import urlparse as compat_urlparse
 
-# Disable certificate verification on python 2.7.9
-if version_info >= (2, 7, 9):
-	try:
-		import ssl
-		sslContext = ssl._create_unverified_context()
-	except:
-		sslContext = None
+try:
+	import ssl
+	sslContext = ssl._create_unverified_context()
+except:
+	sslContext = None
 
 
 def _parse_qsl(qs):
